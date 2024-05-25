d729d194ef6c33b7b0bd17ddb04cfff882623238
Abby Arroyo <abbyarroyo@outlook.com>
2024-05-24 19:43:48 -0700

    This code works for Excel.

---
+++ src/main/java/com/csase/ReadExcelData.java
@@ -0,0 +1,78 @@
+package com.csase;
+import org.apache.poi.ss.usermodel.*; // These are for Excel
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.io.File; // This is so I can read the file from the same directory as the code
+
+public class ReadExcelData {
+
+    private static String fileName = "src/main/java/com/csase/HumaneSociety_data.xlsx";
+
+    public static void readDataForArrayLists(List<Cats> catList) {
+        // TODO DOG UPDATE START   public static void readDataForArrayLists(List<Cats> catList, List<Dogs> dogList) {
+
+        try (FileInputStream fileInputStream = new FileInputStream(new File(fileName))) {
+            // Open the workbook
+            Workbook workbook = new XSSFWorkbook(fileInputStream);
+
+            // Get the sheet
+            int sheetNumber = 0; // The first sheet has the data
+            Sheet sheet = workbook.getSheetAt(sheetNumber);
+
+            // Iterate over rows and cells
+            // I convert all numbers to int in case the user does not input an integer
+            // TODO: I am assuming the data is in the correct order, so I should do some error checking.
+            // TODO: I need more error checking in general. Right now I assume the data are correct by
+            //       assuming an item is an actual item, and numbers are numbers.
+            for (Row row : sheet) {
+                String type = "";
+                String gender = "";
+                String color = "";
+                int age = -1;
+                int number = -1;
+
+                for (Cell cell : row) {
+                    // Check the cell type set the variables
+                    if (cell.getCellType() == CellType.STRING) {
+                        if (type.isEmpty()) {
+                            type = cell.getStringCellValue();
+                        } else if (gender.isEmpty()) {
+                            gender = cell.getStringCellValue();
+                        } else if (color.isEmpty()) {
+                            color = cell.getStringCellValue();
+                        }
+                    } else if (cell.getCellType() == CellType.NUMERIC) {
+                        if (age == -1) {
+                            age = (int) cell.getNumericCellValue();
+                        } else if (number == -1) {
+                            number = (int) cell.getNumericCellValue();
+                        }
+                    }
+                }
+
+                // Check if the data are there and create the object
+                if (!type.isEmpty() && !gender.isEmpty() && !color.isEmpty()) {
+                    if (age >= 0 && number >= 0 ) {
+                        if (type.equals("CAT")) {
+                            Cats catItem = new Cats(type, gender, color, age, number);
+                            catList.add(catItem);
+                        }
+                        // TODO DOG UPDATE START
+                        //else if (type.equals("DOG")){
+                        //
+                        //}
+                    }
+                }
+            }
+
+            // Close the workbook
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
